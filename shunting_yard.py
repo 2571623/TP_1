94ed03689cab2ca7cecb53326fce900cdcaeda27
Larry <lquach0511@gmail.com>
2026-02-27 23:20:35 -0500

    Version 2- infix_to_postfix

--- shunting_yard.py
+++ shunting_yard.py
@@ -14,16 +14,50 @@ def tokenize(expression: str) -> list[str]:
             start = i  # mémorise la position où le nombre commence dans la chaîne
             while i < len(expression) and (expression[i].isdigit() or expression[i] == "."):
                 i += 1
-            tokens.append(expression[start:i])
-
-        else:
+            nombre = (expression[start:i])
+            if nombre.count(".") > 1:
+                raise ValueError(f"Nombre mal formé : {nombre}")
+            tokens.append(nombre)
+        elif expression[i] in "+-*/()": 
             tokens.append(expression[i])
             i += 1
+
+        else:
+            raise ValueError(f"Opérateur inconnu : {expression[i]}")
 
     return tokens
     
 def infix_to_postfix(tokens: list[str]) -> list[str]:
-    wewe
+    
+    priorite = {'+': 1, '-': 1, '*': 2, '/': 2}
+
+    output = []
+    stack_operateur = []
+
+    for token in tokens:
+        if token not in "+-*/()":
+            output.append(token)
+
+        elif token in "+-*/":
+            while (stack_operateur and stack_operateur[-1] in priorite and priorite[stack_operateur[-1]] >= priorite[token]):
+                output.append(stack_operateur.pop())
+
+            stack_operateur.append(token)
+
+        elif token =="(":
+            stack_operateur.append(token)
+
+        elif token == ")":
+            while stack_operateur and stack_operateur[-1] != "(":
+                output.append(stack_operateur.pop())
+            if not stack_operateur:
+                raise ValueError("Parenthèses non appariées : ) sans (")
+            stack_operateur.pop()  # Retire la parenthèse ouvrante
+
+    while stack_operateur:
+        if stack_operateur[-1] in "()":
+            raise ValueError("Parenthèses non appariées : ( sans )")
+        output.append(stack_operateur.pop())
 
 
 def evaluate_postfix(tokens: list[str]) -> float:
